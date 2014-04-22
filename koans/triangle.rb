f20b09daabd1202afb423e0f525178f2a027e0a6
Ashton Thomas <ashton@acrinta.com>
2014-04-22 10:21:41 -0400

    adding true-false and triangle code

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,33 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  s = 0
+
+  if a == b
+    s+=1
+  end
+
+  if b == c
+    s+=1
+  end
+
+  if a == c
+    s+=1
+  end
+
+
+  if s == 0
+    return :scalene
+  elsif s == 1
+    return :isosceles
+  elsif s == 2 || s == 3
+    return :equilateral
+  else
+    return nil
+  end
+
+
+
 end
 
 # Error class used in part 2.  No need to change this code.
